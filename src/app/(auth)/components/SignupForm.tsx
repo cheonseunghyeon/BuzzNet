60a170a5fcb0fd84430462463f600b8ed18e57c4
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2024-10-20 20:23:17 +0900

    Feat: 회원가입 기능 구현

--- src/app/(auth)/components/SignupForm.tsx
+++ src/app/(auth)/components/SignupForm.tsx
@@ -1,6 +1,9 @@
 "use client";
 import React, { useState } from "react";
 import FormInput from "./FormInput";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "@/firebase/init";
 
 export const SignupForm = () => {
   const [email, setEmail] = useState("");
@@ -8,9 +11,32 @@ export const SignupForm = () => {
   const [password, setPassword] = useState("");
   const [confirmpassword, setConfirmpassword] = useState("");
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("회원가입 시도:", { email, name, password, confirmpassword });
+
+    if (password !== confirmpassword) {
+      alert("Passwords do not match."); // 후에 toast로 변경 예정
+      return;
+    }
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      console.log("User signed up and data stored in Firestore:", user.uid);
+
+      await setDoc(doc(db, "users", user.uid), {
+        uid: user.uid,
+        email: user.email,
+        name: name,
+        createdAt: new Date(),
+      });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      } else {
+        console.error("Unknown error occurred");
+      }
+    }
   };
 
   return (
