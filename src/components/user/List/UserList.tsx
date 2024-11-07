130d4e8a51f1c4aa2637e49b6b27bab0626e8dee
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2024-11-07 14:21:29 +0900

    Fix: 빌드 에러 추가 수정

--- src/components/user/List/UserList.tsx
+++ src/components/user/List/UserList.tsx
@@ -8,12 +8,12 @@ const UserList = ({ users }: { users: UserType[] }) => (
       <Link href={`/profiles/${user.uid}`} key={user.uid}>
         <div className="hover:bg-gray-100 p-4 rounded-lg flex items-center justify-between space-x-4 cursor-pointer">
           <div className="flex items-center space-x-4">
-            {user.imageUrl ? (
+            {/* {user.imageUrl ? (
               <img src={user.imageUrl} alt={`${user.name}'s profile`} className="w-14 h-14 rounded-full " />
             ) : (
               <div className="w-14 h-14 rounded-full bg-gray-200" />
-            )}
-
+            )} */}
+            <div className="w-14 h-14 rounded-full bg-gray-200" />
             <div>
               <span className="font-bold text-lg">{user.name}</span>
               <div className="text-sm text-gray-600">
